685bd17f40f79aad7bed813ebc905f881dddc0ee
danshkk <shkurkodan@gmail.com>
2021-04-17 20:39:49 +0300

    fin

--- js/index.js
+++ js/index.js
@@ -2,51 +2,40 @@ var btnPlus = document.getElementById('btnPlus');
 var btnMinus = document.getElementById('btnMinus');
 var btnMultiply = document.getElementById('btnMultiply');
 var btnSplit = document.getElementById('btnSplit');
-
 var input1 = document.getElementById('num1');
 var input2 = document.getElementById('num2');
 
-function getNumber1() {
-    return Number(input1.value);
-}
-function getNumber2() {
-    return Number(input2.value);
-}
 function makeOperation (operation) {
+    var number1 = Number(input1.value);
+    var number2 = Number(input2.value);
     if(operation === '+') {
-        var result = getNumber1() + getNumber2();
+        var result = number1 + number2;
         window.alert(result);
     }
     else if (operation === '-') {
-        var result = getNumber1() - getNumber2();
+        var result = number1 - number2;
         window.alert(result);
     } 
     else if (operation === '*') {
-        var result = getNumber1() * getNumber2();
+        var result = number1 * number2;
         window.alert(result);
     }
     else if (operation === '/') {
-        var result = getNumber1() / getNumber2();
+        var result = number1 / number2;
         window.alert(result);
     } 
     else {
         window.alert('Unknowm operation');
     }
 }
-function btnPlusClick() {
-    makeOperation('+');
-}
-function btnMinusClick() {
-    makeOperation('-');
-}
-function btnMultiplyClick() {
-    makeOperation('*');
-}
-function btnSplitClick() {
-    makeOperation('/');
+
+function operationOnBtnClick(eventObject) {
+    var event = eventObject.currentTarget;
+    var opera = event.innerHTML;
+    makeOperation(opera);
 }
 
-btnPlus.addEventListener('click', btnPlusClick);
-btnMinus.addEventListener('click', btnMinusClick);
-btnMultiply.addEventListener('click', btnMultiplyClick);
-btnSplit.addEventListener('click', btnSplitClick);
+btnPlus.addEventListener('click', operationOnBtnClick);
+btnMinus.addEventListener('click', operationOnBtnClick);
+btnMultiply.addEventListener('click', operationOnBtnClick);
+btnSplit.addEventListener('click', operationOnBtnClick);
