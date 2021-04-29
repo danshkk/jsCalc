298871c3225817d63a42f0ae2215a930cf1d3eaf
danshkk <shkurkodan@gmail.com>
2021-04-29 14:46:52 +0300

    calc

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
-var btnPlus = document.getElementById('btnPlus');
-var btnMinus = document.getElementById('btnMinus');
-var btnMultiply = document.getElementById('btnMultiply');
-var btnSplit = document.getElementById('btnSplit');
 var input1 = document.getElementById('num1');
 var input2 = document.getElementById('num2');
+var operationButtons = document.getElementsByClassName('operationButtons');
 
 function makeOperation (operation) {
     var number1 = Number(input1.value);
@@ -34,8 +31,6 @@ function operationOnBtnClick(eventObject) {
     var opera = event.innerHTML;
     makeOperation(opera);
 }
-
-var operationButtons = [btnPlus, btnMinus, btnMultiply, btnSplit];
 
 for (var i = 0; i < operationButtons.length; i++) {
     var button = operationButtons[i];
